f39b8c8c87138a63b5ddcb29eb8ac007d679e74f
Julian Naranjo <julian.naranjom@gmail.com>
2023-11-15 12:58:19 -0500

    Add Basic validations

--- ip_addresses_tests.py
+++ ip_addresses_tests.py
@@ -1,18 +1,24 @@
 """ Function to validate IP addresses on data models """
 
-from netaddr import *
+from netaddr import IPAddress
 import yaml
 
-models = ['aaa', 'dns', 'syslog']
+models = [
+    {"service": "aaa", "filename": "aaa"},
+    {"service": "dns", "filename": "dns"},
+    {"service": "syslog", "filename": "syslog"},
+    {"service": "ntp", "filename": "ntp_core"},
+    {"service": "ntp", "filename": "ntp_edge"},
+]
 
 for model in models:
-    with open("config_contexts/" + model + ".yml", 'r') as data_model:
+    with open("config_contexts/" + model["filename"] + ".yml", "r") as data_model:
         data_model = yaml.safe_load(data_model)
     try:
-        for address in data_model[model]['servers']:
-            result = IPAddress(address['ip'])
-            assert str(address['ip']) == str(result)
+        for address in data_model[model["service"]]["servers"]:
+            assert str(address["ip"]) == str(IPAddress(address["ip"]))
+        print(f"No Validation errors found for: {model['service'].upper()}")
     except:
-        print(f"IP address validation failed for: {model.upper()} IP: {str(address['ip'])}")
-
-print ("No Validation errors found.")
+        print(
+            f"IP address validation failed for: {model['service'].upper()} IP: {str(address['ip'])}"
+        )
