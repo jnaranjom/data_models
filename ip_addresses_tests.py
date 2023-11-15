2122f7f2d33d03779f4074fb6e1928d1e6ea2d03
Julian Naranjo <julian.naranjom@gmail.com>
2023-11-15 10:30:36 -0500

    Add Python script for IP addresses testing

---
+++ ip_addresses_tests.py
@@ -0,0 +1,16 @@
+""" Function to validate IP addresses on data models """
+
+from netaddr import *
+import yaml
+
+models = ['aaa', 'dns', 'syslog']
+
+for model in models:
+    with open("config_contexts/" + model + ".yml", 'r') as data_model:
+        data_model = yaml.safe_load(data_model)
+    try:
+        for address in data_model[model]['servers']:
+            result = IPAddress(address['ip'])
+            assert str(address['ip']) == str(result)
+    except:
+        print(f"IP address validation failed for: {model.upper()} IP: {str(address['ip'])}")
